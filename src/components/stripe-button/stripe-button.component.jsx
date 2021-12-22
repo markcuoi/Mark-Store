e3b29293a662a9452336cab767fd4d2955825a79
thanhph4n <thanhphan10397@gmail.com>
2021-12-22 10:22:47 +0700

    fix button payment

--- src/components/stripe-button/stripe-button.component.jsx
+++ src/components/stripe-button/stripe-button.component.jsx
@@ -14,10 +14,10 @@ const StripeCheckoutButton = ({ price }) => {
   return (
     <StripeCheckout
       label="Pay Now"
-      name="CRWN Clothing Ltd."
+      name="Mark Store"
       billingAddress
       shippingAddress
-      image="https://svgshare.com/i/CUz.svg"
+      // image="https://svgshare.com/i/CUz.svg"
       description={`Your total is $${price}`}
       amount={priceForStripe}
       panelLabel="Pay Now"
